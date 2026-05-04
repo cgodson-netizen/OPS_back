a6f5263973773003ae756c6b61c543143939d6eb
Clement <godsonclement3456@gmail.com>
2026-05-04 14:34:11 +0000

    Auto create superuser on deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+echo "from accounts.models import CustomUser; CustomUser.objects.filter(email='godsonclement@3456').exists() or CustomUser.objects.create_superuser('godsonclement@3456', '2vxiohq8', first_name='Admin', last_name='OPS')" | python manage.py shell
